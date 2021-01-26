79db13de64bcf52c6e184c99d57a4b6f2303c380
Muhammad <muhammad.ali.72.27@gmail.com>
2021-01-25 21:40:35 -0800

    added login and deck menu

---
+++ app/src/main/java/com/moehaemad/structuredflashcards/DeckMenu.java
@@ -0,0 +1,14 @@
+package com.moehaemad.structuredflashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeckMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deck_menu);
+    }
+}
--- app/src/main/java/com/moehaemad/structuredflashcards/MainActivity.java
+++ app/src/main/java/com/moehaemad/structuredflashcards/MainActivity.java
@@ -2,7 +2,10 @@ package com.moehaemad.structuredflashcards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected View.OnClickListener startingDeck = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent startDeck = new Intent(Intent.ACTION_VIEW);
+            startDeck.setClass(getApplicationContext(), DeckMenu.class);
+
+            startActivity(startDeck);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button startDeck = findViewById(R.id.start_deck);
+        startDeck.setOnClickListener(startingDeck);
     }
 }
