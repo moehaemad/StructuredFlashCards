c2ca54f02ff112622456464f4d4f95cb3f860f37
Muhammad <muhammad.ali.72.27@gmail.com>
2021-01-27 21:52:22 -0800

    boiler plate for creating and querying users

---
+++ app/src/main/java/com/moehaemad/structuredflashcards/User.java
@@ -0,0 +1,33 @@
+package com.moehaemad.structuredflashcards;
+
+import java.util.HashMap;
+
+public class User {
+    private String name;
+    private int id;
+    private int[] deckId;
+    private HashMap<Integer, String> deck;
+
+    public User(String name, int id){
+        this.name = name;
+        this.id = id;
+        this.deckId = getDeckIds();
+    }
+
+    private int[] getDeckIds(){
+    //    TODO: query database for array of deck ids attached to a user using user id
+        int[] ids = {};
+        return ids;
+    };
+
+    private void pairDeckIdNames(){
+    //    TODO: for each item in the deck id arrays attach the associated name to the hashmap
+    }
+
+    protected HashMap<Integer, String> getDecks(){
+        //TODO: pair each deck id to it's associated string
+        return this.deck;
+    }
+
+
+}
