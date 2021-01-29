5117901daccea8292cee9171ebe2e2aca11b5991
Muhammad <muhammad.ali.72.27@gmail.com>
2021-01-28 16:06:35 -0800

    Implemented onClickListener Interface

--- app/src/main/java/com/moehaemad/structuredflashcards/MainActivity.java
+++ app/src/main/java/com/moehaemad/structuredflashcards/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +26,40 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    protected View.OnClickListener startingUserView = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent startDeck = new Intent(Intent.ACTION_VIEW);
+            startDeck.setClass(getApplicationContext(), DeckMenu.class);
+
+            startActivity(startDeck);
+        }
+    };
+
     @Override
     protected void onResume() {
         super.onResume();
-        Button startDeck = findViewById(R.id.start_deck);
-        startDeck.setOnClickListener(startingDeck);
+        Button startDeck = findViewById(R.id.main_start_deck);
+        startDeck.setOnClickListener(this);
+        Button loginView = findViewById(R.id.main_login);
+        loginView.setOnClickListener(this);
     }
 
+    public void changeView(Class toStart){
+        Intent toStartView = new Intent (Intent.ACTION_VIEW);
+        toStartView.setClass(getApplicationContext(), toStart);
+        startActivity(toStartView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.main_start_deck:
+                changeView(DeckMenu.class);
+                break;
+            case R.id.main_login:
+                changeView(UserMenu.class);
+                break;
+        }
+    }
 }
