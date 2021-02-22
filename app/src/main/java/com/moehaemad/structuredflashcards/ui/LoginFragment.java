b210256abd0e0674be0bda03473267bfe9707f64
Muhammad <muhammad.ali.72.27@gmail.com>
2021-02-21 19:42:15 -0800

    adding login functionality with api

--- app/src/main/java/com/moehaemad/structuredflashcards/ui/LoginFragment.java
+++ app/src/main/java/com/moehaemad/structuredflashcards/ui/LoginFragment.java
@@ -1,9 +1,13 @@
 package com.moehaemad.structuredflashcards.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +26,25 @@ public class LoginFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // please refer to DeckFragment for explanation why attachToRoot = false
         View root = inflater.inflate(R.layout.fragment_login_menu, container, false);
+        Button submitButton = root.findViewById(R.id.login_menu_submit);
+        submitButton.setOnClickListener(this.buttonListener);
+
         return root;
+    }
+
+    protected View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d("onClick button", "the onclick listener is working correctly");
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EditText userInfo = getView().findViewById(R.id.login_menu_username);
+        EditText passInfo = getView().findViewById(R.id.login_menu_password);
+
+
     }
 }
