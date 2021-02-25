fc17ee635cc1e30f86d7491b62b28c0b9e1b99f5
Muhammad <muhammad.ali.72.27@gmail.com>
2021-02-25 13:41:23 -0800

    refactored to only add a request to a queue and not implementing in model

--- app/src/main/java/com/moehaemad/structuredflashcards/model/NetworkRequest.java
+++ app/src/main/java/com/moehaemad/structuredflashcards/model/NetworkRequest.java
@@ -10,13 +10,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class NetworkRequest {
-    public static Context appContext;
-    public RequestQueue requestQueue;
+public class NetworkRequest implements Response.Listener<JSONObject>{
+    protected static Context appContext;
+    protected RequestQueue requestQueue;
     protected int httpType;
     private String websiteEndpoint;
+    public static JSONObject websiteResponse;
 
     public NetworkRequest(Context appContext){
         this.appContext = appContext;
@@ -26,6 +28,20 @@ public class NetworkRequest {
         //load the url to append api endpoint and not to retype domain each time
         this.websiteEndpoint = "https://moehaemad.ca/structuredFlashCards/";
     }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        this.websiteResponse = response;
+    }
+
+    class WebsiteResponse implements com.android.volley.Response.Listener<JSONObject> {
+        @Override
+        public void onResponse(JSONObject response) {
+            websiteResponse = response;
+            Log.d("innerClass webResp", response.toString());
+        }
+    }
+
 
     //create the queue for network calls as per Volley library
     private RequestQueue getRequestQueue (){
@@ -50,7 +66,16 @@ public class NetworkRequest {
         }
     }
 
-
+    public JSONObject getResponse(){
+        try{
+            if (this.websiteResponse == null){
+                this.websiteResponse = new JSONObject("{result: false}");
+            }
+        }catch(JSONException e){
+            Log.d("NetRes getResp", e.toString());
+        }
+        return this.websiteResponse;
+    }
 
     public void getRequest (String endpoint, final Network <JSONObject> listener){
         this.websiteEndpoint += endpoint;
@@ -72,6 +97,40 @@ public class NetworkRequest {
                 this.websiteEndpoint,
                 null,
                 response, error));
+    }
+
+    public void addToRequestQueue(Request<JSONObject> request){
+        getRequestQueue().add(request);
+    }
+
+/*    public void getRequest(String endpoint){
+        this.websiteEndpoint += endpoint;
+        Response.ErrorListener error = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Network Error", "request failed");
+            }
+        };
+        this.requestQueue.add(new JsonObjectRequest(this.httpType,
+                this.websiteEndpoint,
+                null,
+                new WebsiteResponse(), error));
+        Log.d("NetReq getReq end", this.websiteResponse.toString());
+    }*/
+
+    public void getRequest(String endpoint){
+        this.websiteEndpoint += endpoint;
+        Response.ErrorListener error = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Network Error", "request failed");
+            }
+        };
+        this.requestQueue.add(new JsonObjectRequest(this.httpType,
+                this.websiteEndpoint,
+                null,
+                this, error));
+        Log.d("NetReq getReq end", this.websiteResponse.toString());
     }
 
 }
