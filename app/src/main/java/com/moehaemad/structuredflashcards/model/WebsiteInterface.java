d9f93f60ba2388b110de0420e0e359a031fc5436
Muhammad <muhammad.ali.72.27@gmail.com>
2021-02-27 11:37:31 -0800

    got local host to work in development

--- app/src/main/java/com/moehaemad/structuredflashcards/model/WebsiteInterface.java
+++ app/src/main/java/com/moehaemad/structuredflashcards/model/WebsiteInterface.java
@@ -1,19 +1,17 @@
 package com.moehaemad.structuredflashcards.model;
 
-import com.android.volley.Response;
-
 public interface WebsiteInterface {
     String USER_VALIDATED = Preferences.USER_VALIDATED;
     String USER_NAME = Preferences.USER_NAME;
 
 //    String PRODUCTION = "https://moehaemad.ca/structuredFlashCards/";
-    String PRODUCTION = "http://localhost:3000/";
+    String PRODUCTION = "http://192.168.1.65:3000/structuredFlashCards/";
     String CREATE_USER = PRODUCTION + "createUser";
     String CREATE_DECK = PRODUCTION + "createDeck";
     String CREATE_CARD = PRODUCTION + "createCard";
-    String CHECK_USER = PRODUCTION + "checkuser";
-    String GET_DECKS = PRODUCTION + "getDecks";
-    String GET_CARDS = PRODUCTION + "getCards";
+    String CHECK_USER = PRODUCTION + "checkuser/";
+    String GET_DECKS = PRODUCTION + "getDecks/";
+    String GET_CARDS = PRODUCTION + "getCards/";
     String SET_CARD = PRODUCTION + "setCard";
     String SET_DECK = PRODUCTION + "setDeck";
     String DELETE_CARD = PRODUCTION + "delCard";
