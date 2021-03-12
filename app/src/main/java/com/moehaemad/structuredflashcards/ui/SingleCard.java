57659d99a2bd33acfee48c6e8e5e5a844f46d9ab
Muhammad <muhammad.ali.72.27@gmail.com>
2021-03-11 22:00:38 -0800

    added single card view and changed ids

---
+++ app/src/main/java/com/moehaemad/structuredflashcards/ui/SingleCard.java
@@ -0,0 +1,40 @@
+package com.moehaemad.structuredflashcards.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.moehaemad.structuredflashcards.R;
+
+public class SingleCard extends Fragment {
+
+
+    public SingleCard() {
+        // Required empty public constructor
+    }
+
+    public static SingleCard newInstance(String param1, String param2) {
+        SingleCard fragment = new SingleCard();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_single_card, container, false);
+    }
+}
