80243df86d24ce708429f44225a92a430030858f
Muhammad <muhammad.ali.72.27@gmail.com>
2021-03-18 15:21:37 -0700

    added deck id to string method and documentation

--- app/src/main/java/com/moehaemad/structuredflashcards/controller/Deck.java
+++ app/src/main/java/com/moehaemad/structuredflashcards/controller/Deck.java
@@ -18,12 +18,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.LinkedList;
+
 public class Deck {
     private String username;
     private JSONArray deckArray;
     private SharedPreferences sharedPreferences;
     private Context ctx;
 
+    /**
+     * Create deck from context without specifying username in case it's presumed to be set.
+     * */
     public Deck(Context ctx){
         this.sharedPreferences = ctx.getSharedPreferences(Preferences.PACKAGE, Context.MODE_PRIVATE);
         this.ctx = ctx;
@@ -31,6 +36,9 @@ public class Deck {
         setDeck();
     }
 
+    /**
+    * Create deck and synchronise the shared preferences in case changes to ids have been made.
+    * */
     public Deck(@NonNull Context ctx, @NonNull  String username){
         this(ctx);
         this.username = username;
@@ -38,24 +46,35 @@ public class Deck {
         syncDeck();
     }
 
+    /**
+     * Class used for a generic network request. Intead of anonymous class, this inner class sets
+     *  the shared preferences .
+     * */
     class GetDeckResponse implements Response.Listener<JSONObject>{
         @Override
+
+        /**
+         * Handle the response from the network request.
+         * */
         public void onResponse(JSONObject response) {
+            //open shared preferences editor
             SharedPreferences.Editor prefEditor = sharedPreferences.edit();
-            Log.d("Deck response", response.toString());
-            //TODO: put the list of deck id's in here
-            //TODO: check the output received as JSON request
             try{
+                // add response as string from json array into shared preferences
                 prefEditor.putString(Preferences.DECK_ARRAY, response.get("ids").toString());
                 Log.d("Deck ids", response.get("ids").toString());
                 Log.d("deckArray", deckArray.toString());
             }catch(JSONException e){
                 Log.e("Deck json error", e.getMessage());
             }
+            //apply changed from the editor
             prefEditor.apply();
         }
     }
 
+    /**
+     * Generic Error response.
+     * */
     private Response.ErrorListener error = new Response.ErrorListener(){
         @Override
         public void onErrorResponse(VolleyError error) {
@@ -64,6 +83,9 @@ public class Deck {
     };
 
 
+    /**
+     * Create a network request to get the ids from the API.
+     * */
     public void syncDeck(){
         //create network request for GET of the ids
         NetworkRequest networkRequest = new NetworkRequest(this.ctx);
@@ -77,7 +99,9 @@ public class Deck {
                 new GetDeckResponse(),
                 error));
     }
-
+    /**
+     * Set the deck Id's from shared preferences into class variable.
+     * */
     private void setDeck(){
         //get deck ids from shared preferences
         String decks = this.sharedPreferences.getString(Preferences.DECK_ARRAY, "[]");
@@ -94,9 +118,38 @@ public class Deck {
         }
     }
 
-
+    /**
+     * Grab the ids from class property
+     * */
     public JSONArray getDeckIds(){
         return this.deckArray == null ? new JSONArray() : this.deckArray;
     };
+
+    /**
+     * Returns the deck ids from the network request as strings instead of JSONArray to avoid parsing
+     *  the JSONArray in UI.
+     * */
+    public LinkedList<String> getDeckIdsAsString(){
+        //create empty list
+        LinkedList<String> toReturn = new LinkedList<>();
+        //get deck ids from network request as JSONArray
+        JSONArray jsonIds = this.getDeckIds();
+        //check if null
+        if (jsonIds == null) return toReturn;
+        try{
+            for (int i=0, size=jsonIds.length(); i < size; i++){
+                //grab the object each item of the array holds
+                JSONObject jsonObject = jsonIds.getJSONObject(i);
+                //add the object's id property as a string into the returned list
+                toReturn.add(jsonObject.getString("id"));
+            }
+            //return list
+            return toReturn;
+        }catch (JSONException e){
+            Log.e("Deck getDeckIdsAsString", e.getMessage());
+        }
+        //used for convenience but function will not enter this stage because of null check.
+        return toReturn;
+    }
 
 }
