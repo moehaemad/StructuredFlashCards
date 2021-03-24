64884fa0206a2fa56f2af96619d651224f75b7c6
Muhammad <muhammad.ali.72.27@gmail.com>
2021-03-23 23:38:01 -0700

    added method to send body to network request

--- app/src/main/java/com/moehaemad/structuredflashcards/controller/FlashCard.java
+++ app/src/main/java/com/moehaemad/structuredflashcards/controller/FlashCard.java
@@ -7,12 +7,14 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.moehaemad.structuredflashcards.model.NetworkRequest;
 import com.moehaemad.structuredflashcards.model.Preferences;
+import com.moehaemad.structuredflashcards.model.UserInput;
 import com.moehaemad.structuredflashcards.model.WebsiteInterface;
 import com.moehaemad.structuredflashcards.ui.DeckFragment;
 
@@ -21,6 +23,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class FlashCard{
@@ -36,6 +39,8 @@ public class FlashCard{
      * */
     public FlashCard(@NonNull Context appContext){
         this.appContext = appContext;
+        //add shared preferences here because it is crucial to everything else
+        this.sharedPreferences = appContext.getSharedPreferences(prefName, Context.MODE_PRIVATE);
     }
 
     /**
@@ -46,7 +51,6 @@ public class FlashCard{
         this.id=id;
         this.question = "";
         this.answer = "";
-        this.sharedPreferences = appContext.getSharedPreferences(prefName, Context.MODE_PRIVATE);
         setupCards();
     }
 
@@ -104,11 +108,13 @@ public class FlashCard{
     /**
      * On change or creating the deck id, set the id into shared preferences and the class variable
      * */
-    private void setActiveId(int id){
+    public void setActiveId(int id){
         this.id = id;
         SharedPreferences.Editor editor = this.sharedPreferences.edit();
         editor.putInt(Preferences.ACTIVE_DECK_ID, id);
         editor.apply();
+        //if the active id has changed then the cards being retrieved will need to be queried
+        this.setupCards();
     }
 
     /**
@@ -117,10 +123,11 @@ public class FlashCard{
     public int getActiveId(){
         if (this.id == -1){
             //if id is not set which it should be but in the case it isn't grab from shared preferences.
-            return this.sharedPreferences.getInt(Preferences.ACTIVE_DECK_ID, -1);
-        }else{
+            //check if the shared preferences even has a deck id to begin with
+            this.id = this.sharedPreferences.getInt(Preferences.ACTIVE_DECK_ID, -1);
             return this.id;
         }
+        return this.id;
     }
 
 
@@ -163,14 +170,68 @@ public class FlashCard{
         }
     }
 
-    public void deleteCard(final WebsiteInterface.WebsiteResult verification, int id){
+    public void updateCard (final WebsiteInterface.WebsiteResult verification,
+                            HashMap<String, String> updateParams){
+        //Convert parameters to json acceptable to communicate
+        JSONObject jsonBody = jsonifyParams(updateParams);
+        //set the standard response listener with the verification interface that was given.
+        Response.Listener<JSONObject> updateCard = setResponse(verification);
+        //create endpoint
+        String api = WebsiteInterface.SET_CARD;
+        NetworkRequest networkRequest = new NetworkRequest(this.appContext);
+        int method = networkRequest.getMethod("PUT");
+
+
+    }
+
+    /**
+     * Convert Hasmap values into the JSON format that is expected in update request.w
+     * */
+    private JSONObject jsonifyParams(HashMap<String, String> params){
+        try {
+            //first create the initial JSONObject
+            JSONObject jsonBody = new JSONObject();
+            //enter in columns and specify columns as JSONArray each
+            //TODO: make code cleaner by sending to for-loop to iterate with different id's
+            //new columns
+            JSONArray columns = new JSONArray();
+            //set the id ex {"id": 0}
+            columns.put(new JSONObject("{\"id\":" + params.get(WebsiteInterface.UPDATE_ID) + " }"));
+            //set the front ex {"front":... }
+            columns.put(new JSONObject("{\"front\":" + params.get(WebsiteInterface.UPDATE_FRONT) + "}"));
+            //set the back ex {"back" :...}
+            columns.put(new JSONObject("{\"back\":" + params.get(WebsiteInterface.UPDATE_BACK) + "}"));
+
+            //specify columns
+            JSONArray specifyColumns = new JSONArray();
+            //set the id ex {"id": 0}
+            columns.put(new JSONObject("{\"id\":" + params.get(WebsiteInterface.UPDATE_ID) + " }"));
+            //set the front ex {"front":... }
+            columns.put(new JSONObject("{\"front\":" + params.get(WebsiteInterface.PREV_FRONT) + "}"));
+            //set the back ex {"back" :...}
+            columns.put(new JSONObject("{\"back\":" + params.get(WebsiteInterface.PREV_BACK) + "}"));
+            //attach the json arrays into the returning body
+            jsonBody.put("columns", columns);
+            jsonBody.put("specifyColumns", specifyColumns);
+            /*return this json body if nothing went wrong with json formatting, otherwise empty json
+            *   object.*/
+            return jsonBody;
+
+        } catch (JSONException e) {
+            Log.e("FCard jsonifyParams", e.getMessage());
+        }
+        return new JSONObject();
+    }
+
+    public void deleteCard(final WebsiteInterface.WebsiteResult verification,
+                           int id, String front, String back){
         //create the standard response for the UI
         Response.Listener<JSONObject> deleteCard = setResponse(verification);
         //isntantiate network request object
         NetworkRequest networkRequest = new NetworkRequest(this.appContext);
         //set api endpoint; use /:id/:front?/:back?
-        //TODO: receive value of front and back as well but under different method signature
-        String api = WebsiteInterface.DELETE_CARD + String.valueOf(id);
+        //TODO: change the value here to incluse the fron and back
+        String api = WebsiteInterface.DELETE_CARD + String.valueOf(id) + "/" + front + "/" + back;
         //set method
         int method = networkRequest.getMethod("DELETE");
         //send the network request off into queue
@@ -222,6 +283,7 @@ public class FlashCard{
             return new LinkedList<JSONObject>();
         }
         try{
+            //TODO: create sync function to gather cards from api and not in shared preferences
             //Create JSON array object from preferences string
             JSONArray listOfCards = new JSONArray(cardsAsString);
             //the array contains a size of the number of cards and each card has a front and back
@@ -243,7 +305,4 @@ public class FlashCard{
         //should not get here because of JSON iteration. Used for default
         return new LinkedList<JSONObject>();
     }
-
-
-
 }
