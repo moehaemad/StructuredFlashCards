aa2a95c4a23d8eb436f5d8c25cff2421bb16f4d5
Muhammad <muhammad.ali.72.27@gmail.com>
2021-04-16 19:03:51 -0700

    added view to create a deck

---
+++ app/src/main/java/com/moehaemad/structuredflashcards/ui/CreateDeckFragment.java
@@ -0,0 +1,36 @@
+package com.moehaemad.structuredflashcards.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.moehaemad.structuredflashcards.R;
+
+public class CreateDeckFragment extends Fragment {
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public CreateDeckFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_deck, container, false);
+    }
+
+}
